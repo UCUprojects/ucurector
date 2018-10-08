838691215fb6b208e8c175def257cf4169453316
Jarko Umbrelluck <35334179+umbrelluck@users.noreply.github.com>
2018-10-08 20:38:07 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // js-файл з основними функціями та змінними
 // ще ніде не використовується
-var love, religion, budget, prestige
-love = religion = budget = prestige = 50
+var love, religion, budget, prestige, reputation
+love = religion = budget = prestige = reputation = 50
 $(function () {
     nextTurn();
 })
 
 function nextTurn() {
-    while (love > 0 & love < 100 & religion > 0 & religion < 100 & budget > 0 & budget < 100 & prestige > 0 & prestige < 100) {
+    while (love > 0 & love < 100 & religion > 0 & religion < 100 & budget > 0 & budget < 100 & prestige > 0 & prestige < 100 & reputation > 0 & reputation < 100) {
         // якась подія 
         // якась рандомна подія (рандомніша за попередню, типу прорвало трубу і затопило трапезну)
     }
